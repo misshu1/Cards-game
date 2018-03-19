bc8dcc5f43d465f8f2b70b379cc70f91ba839d43
misshu1 <31734905+misshu1@users.noreply.github.com>
2018-03-19 19:06:41 +0200

    small change

--- js/app.js
+++ js/app.js
@@ -125,7 +125,6 @@ function restart() {
     thirdStar.style.color = '';
     time.innerText = '0 : 0';
     openCardsArray = [];
-    console.log(openCardsArray);
     count = 0;
     seconds = 1;
     minutes = 0;
